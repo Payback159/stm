e45c739b6e915c077ec2ed29d77635254f390d28
Payback159 <payback159@gmail.com>
2020-05-10 23:24:27 +0200

    added some reliability features

--- install_speedtest_environment.sh
+++ install_speedtest_environment.sh
@@ -1,33 +1,23 @@
 #!/usr/bin/env bash
 
-ENV_DIR=speedtest-env
-INFLUXVERS="influxdb_2.0.0-beta.9_linux_arm64.tar.gz"
+set -eo pipefail
 
-mkdir "${ENV_DIR}"
-cd "${ENV_DIR}" || exit
-wget -O speedtest.tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-aarch64-linux.tgz
-tar -xzvf speedtest.tgz
+FILE=speedtest
+DATA_FILE=speedtest-data.csv
 
-#Grafana OSS
-sudo apt-get install -y apt-transport-https
-sudo apt-get install -y software-properties-common wget
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+if [ -f "${FILE}" ]; then
+  echo "Speedtest binary isn't older than 1 day - Skipped download"
+else
+  wget -O "${FILE}".tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-aarch64-linux.tgz
+  tar -xzvf "${FILE}".tgz
+fi
 
-sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+if [ ! -f "${DATA_FILE}" ] && [ -f "${FILE}" ]; then
+    echo "Running initial speedtest to create csv header"
+    ./speedtest --format=csv --output-header > "${DATA_FILE}"
+else
+  echo "${DATA_FILE} exists! - skipping initial speedtest to create csv header"
+fi
 
-sudo apt-get update
-sudo apt-get install grafana
-
-#InfluxDB
-wget https://dl.influxdata.com/influxdb/releases/"${INFLUXVERS}"
-tar --strip-component=1 xvzf "${INFLUXVERS}"
-sudo cp ./{influx,influxd} /usr/local/bin/
-sudo useradd -rs /bin/false influxdb
-sudo cp influxdb2.service /lib/systemd/system
-
-#Start all services
-sudo systemctl daemon-reload
-sudo systemctl start grafana-server.service
-sudo systemctl start influxdb2.service
-sudo systemctl enable grafana-server.service
-sudo systemctl enable influxdb2.service
+echo "Executing docker compose"
+docker-compose up -d
