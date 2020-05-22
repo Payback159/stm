12d7915bce3761bac6e6ef0c279d20bba8b40ed5
Payback159 <payback159@gmail.com>
2020-05-22 16:51:15 +0200

    adding feature to schedule speedtest via speedtest.env

--- speedtest-app/README.md
+++
@@ -1,7 +0,0 @@
-#Build Guide
-
-Normally you should get the latest version with `docker pull payback159/stm:latest`, if you still want to build your image locally you can do that with the resources in this folder.
-
-```bash
-docker build ./speedtest-app --file Dockerfile -t payback159/stm:latest
-```
--- speedtest-app/testing-internet-speed.py
+++ speedtest-app/testing-internet-speed.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import schedule
+import time
 import datetime
 import speedtest
 from influxdb import InfluxDBClient
@@ -11,39 +13,46 @@ try:
    ifdb = os.environ["IF_DB"]
    ifhost = os.environ["IF_HOST"]
    ifport = os.environ["IF_PORT"]
+   minterval = int(os.environ["MEASUREMENT_INTERVAL"])
 except KeyError: 
    print("Please set the needed environment variables")
    sys.exit(1)
 
-measurement_name = "internet"
+def measure_internet():
+  # take a timestamp for this measurement
+  time = datetime.datetime.utcnow()
+  measurement_name = "internet"  
+
+  s = speedtest.Speedtest()
+  s.get_best_server()
+  s.download(threads=threads)
+  s.upload(threads=threads)
+  res = s.results.dict()
+  # format the data as a single measurement for influxdb
+  body = [
+      {
+          "measurement": measurement_name,
+          "time": time,
+          "fields": {
+              "download": res["download"],
+              "upload": res["upload"],
+              "ping": res["ping"]
+          }
+      }
+  ]
+
+  # write the measurement
+  ifclient.write_points(body)
 
 #speedtest configuration
 threads = None
 
-# take a timestamp for this measurement
-time = datetime.datetime.utcnow()
-
-s = speedtest.Speedtest()
-s.get_best_server()
-s.download(threads=threads)
-s.upload(threads=threads)
-res = s.results.dict()
-
-# format the data as a single measurement for influxdb
-body = [
-    {
-        "measurement": measurement_name,
-        "time": time,
-        "fields": {
-            "download": res["download"],
-            "upload": res["upload"],
-            "ping": res["ping"]
-        }
-    }
-]
-
-# connect to influx
+#open connection to database
 ifclient = InfluxDBClient(ifhost,ifport,ifuser,ifpass,ifdb)
 
-# write the measurement
-ifclient.write_points(body)
+#setup scheduler
+schedule.every(minterval).minutes.do(measure_internet)
+
+while 1:
+  schedule.run_pending()
+  time.sleep(1)
